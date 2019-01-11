b9ffd1c945755eb0550860f9d01ed5a2722e32fa
Breno Azevedo <maddox.br@gmail.com>
2019-01-11 03:03:40 -0200

    WIP/TODO: featuredefs: only 'object' & 'featuredead' should have names updated Update weapondamagetypes accordingly Copy over / update scripts

---
+++ objects3d/armoutpost.dae.lua
@@ -0,0 +1,9 @@
+model = {
+      --radius = 25.0,
+      --height = 40,
+      tex1 = "armoutpost_tex.dds",
+      tex2 = "armoutpost_tex2.dds",
+      midpos = {0,0,0},
+      --rotAxisSigns = {-1, -1, -1}
+}
+return model
