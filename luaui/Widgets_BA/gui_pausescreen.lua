fc673cdc85ffe93a0c3bddd759edeb210541ec73
silentwings <3885930+silentwings@users.noreply.github.com>
2019-05-23 00:05:31 +0100

    clean up gui_pausescreen
    possibly fix #153

--- luaui/Widgets_BA/gui_pausescreen.lua
+++ luaui/Widgets_BA/gui_pausescreen.lua
@@ -1,6 +1,3 @@
-include("keysym.h.lua")
-local versionNumber = "1.34"
-
 function widget:GetInfo()
     return {
         name      = "Pause Screen",
@@ -16,10 +13,8 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-
-local initialized = os.clock()
-
 local spGetGameSpeed        = Spring.GetGameSpeed
+local spIsGUIHidden         = Spring.IsGUIHidden
 
 local glColor               = gl.Color
 local glTexture             = gl.Texture
@@ -39,27 +34,90 @@ local glGetUniformLocation  = gl.GetUniformLocation
 local osClock               = os.clock
 
 ----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
--- CONFIGURATION
+
+-------------------------------
+-- logic
+-------------------------------
+
+local slideTime = 0.4
+
+local paused = false
+local lastPaused = false -- pause state on previous Update
+local pauseTimestamp = -slideTime -- most recent time at which pause state changed
+local drawPauseScreen = false
+local gameStarted = false
+
+
+function widget:Initialize()
+  InitializeGL()
+  
+  if Spring.GetGameFrame() > 0 then
+  	gameStarted = true
+  end
+  
+  widget:Update()
+  if paused then
+	pauseTimestamp = -slideTime -- gracefully handle luaui reload whilst paused 
+    lastPaused = true
+  end
+end
+
+function widget:GameStart()
+	gameStarted = true
+end
+
+function widget:GameOver()
+    widgetHandler:RemoveWidget()
+end
+
+function widget:Update(dt)
+	if not gameStarted then return end	
+	
+    local _, gameSpeed, isPaused = spGetGameSpeed()
+    if isPaused or gameSpeed <= 0 then -- SPADS can "pause" by setting gamespeed 0
+        paused = true
+    else
+        paused = false
+    end	
+	
+	local now = osClock()		
+	local became_paused = (paused and not lastPaused)
+	local became_unpaused = (not paused and lastPaused)
+    if became_paused or became_unpaused then
+		local diffPauseTime = now - pauseTimestamp
+        if diffPauseTime <= slideTime then
+			-- pause state changed during animation, re-write history to make the sliding work nicely
+            timeRemaining = slideTime - diffPauseTime
+			pauseTimestamp = now - timeRemaining
+		else 
+			-- "normal" pause state change
+			pauseTimestamp = osClock()
+		end
+    end
+
+	drawPauseScreen = paused or (osClock() - pauseTimestamp <= slideTime)
+	if drawPauseScreen and spIsGUIHidden() then drawPauseScreen = false end
+	
+    lastPaused = paused	
+end
+
+-------------------------------
+-- draw
+-------------------------------
 
 local sizeMultiplier        = 1
 local maxAlpha              = 0.6
-local maxShaderAlpha        = 0.2
+local maxShaderAlpha        = 0.33
 local maxNonShaderAlpha     = 0.1			--background alpha when shaders arent availible
 local boxWidth              = 200
 local boxHeight             = 35
-local slideTime             = 0.4
-local fadeToTextAlpha       = 0.4
+local fadeToTextAlpha       = 0.5
 local fontSizeHeadline      = 24
 local fontPath              = "LuaUI/Fonts/MicrogrammaDBold.ttf"
-local autoFadeTime          = 1
 
-local blurScreen            = false 	-- makes use of guishader api widget
-
-local vsx, vsy
 local myFont
-local pauseTimestamp = -10 --start or end of pause
-local lastPause = false
 local wndX1 = nil
 local wndY1 = nil
 local wndX2 = nil
@@ -68,17 +126,13 @@ local textX = nil
 local textY = nil
 local usedSizeMultiplier = 1
 local vsx, vsy = Spring.GetWindowGeometry()
-local widgetInitTime = osClock()
-local previousDrawScreenClock = osClock()
-local paused = false
 
 local shaderAlpha = 0
 local screencopy
 local shaderProgram
 
-
---intensity formula based on http://alienryderflex.com/hsp.html
 local fragmentShaderSource = {
+	--intensity formula based on http://alienryderflex.com/hsp.html
 	washed = [[
 		uniform sampler2D screencopy;
 		uniform float alpha;
@@ -114,67 +168,20 @@ local fragmentShaderSource = {
 	]],
 }
 
-local gameover = false
-function widget:GameOver()
-    gameover = true
-end
-
-local prevGameFrameTime = osClock()
-function widget:GameFrame(dt)
-    prevGameFrameTime = osClock()
-end
-
-function widget:Update(dt)
-    local now = osClock()
-    previousDrawScreenClock = now
-
-    local diffPauseTime = ( now - pauseTimestamp)
-
-    if ( ( not paused and lastPause ) or ( paused and not lastPause ) ) then
-        --pause switch
-        pauseTimestamp = osClock()
-        if ( diffPauseTime <= slideTime ) then
-            pauseTimestamp = pauseTimestamp - ( slideTime - ( diffPauseTime / slideTime ) * slideTime )
-        end
-    end
-
-    if ( paused and not lastPause ) then
-        --new pause
-        if widgetInitTime + 5 > now then        -- so if you do /luaui reload when paused, it wont re-animate
-            pauseTimestamp = now - (slideTime + autoFadeTime)
-        end
-    end
-
-    lastPause = paused
-
-    local _, _, isPaused = spGetGameSpeed()
-    if os.clock()-initialized > 3 and not gameover and (isPaused or (now - prevGameFrameTime > 1.2 and Spring.GetGameFrame() > 0)) then    -- when host (admin) paused its just gamespeed 0
-        paused = true
-    else
-        paused = false
-    end
-end
-
-function widget:Initialize()
+function InitializeGL()
   vsx, vsy = widgetHandler:GetViewSizes()
   widget:ViewResize(vsx, vsy)
   
-  myFont = glLoadFont( fontPath, fontSizeHeadline )
+  myFont = glLoadFont(fontPath, fontSizeHeadline)
    
-
-  local _, gameSpeed, isPaused = spGetGameSpeed()
-  if isPaused or gameSpeed == 0 then    -- when host admin paused its just gamespeed 0
-      paused = true
-  end
-
   if gl.CreateShader and (Platform == nil or Platform.gpuVendor ~= 'Intel') then
     shaderProgram = gl.CreateShader(
     {
-		  fragment = fragmentShaderSource.washed,
-		  uniformInt = {
-			  screencopy = 0,
-			},
-		})
+	  fragment = fragmentShaderSource.washed,
+	  uniformInt = {
+		  screencopy = 0,
+		},
+	})
     if shaderProgram then
   	    alphaLoc = glGetUniformLocation(shaderProgram, "alpha")
     end
@@ -184,84 +191,54 @@ function widget:Initialize()
 end
 
 function widget:Shutdown()
-  glDeleteFont( myFont )
+  glDeleteFont(myFont)
   if shaderProgram then
     gl.DeleteShader(shaderProgram)
   end
 end
 
-function widget:GamePaused(playerID, isGamePaused)
-    paused = isGamePaused
-end
-
 function widget:DrawScreen()
-  if Spring.IsGUIHidden() then return end
-    local now = osClock()
-    
-    if ( paused or ( ( now - pauseTimestamp) <= slideTime ) ) then
-        showPauseScreen = true
-        drawPause()
-        if blurScreen and WG['guishader_api'] ~= nil then
-            WG['guishader_api'].InsertRect(0,0,vsx,vsy, 'pausescreen')
-        end
-    else
-        showPauseScreen = false
-        if blurScreen and WG['guishader_api'] ~= nil then
-            WG['guishader_api'].RemoveRect('pausescreen')
-        end
-    end
+  if Spring.IsGUIHidden() then return end  
+  if not drawPauseScreen then return end
+  DrawPause()
 end
 
-
-function drawPause()
+function DrawPause()
     local now = osClock()
-    local diffPauseTime = ( now - pauseTimestamp)
+    local diffPauseTime = (now - pauseTimestamp)
     
     local text           = { 1.0, 1.0, 1.0, 0*maxAlpha }
     local outline        = { 0.0, 0.0, 0.0, 0*maxAlpha }     
     
+	-- progress: 1==off-screen, 0==center
     if paused then 
-    	progress = ( now - pauseTimestamp ) / slideTime 
+    	progress = math.max(0.0, 1.0 - diffPauseTime / slideTime)
     else
-    	progress = 1 - (( now - pauseTimestamp ) / slideTime )
+    	progress = math.min(1.0, diffPauseTime / slideTime)
     end
-    if progress > 1 then
-    	progress = 1
-    end
-    if progress < 0 then
-    	progress = 0
-    end
-    text[4]			= (text[4] * (1-progress)) + fadeToTextAlpha
-    outline[4]	= (outline[4] * (1-progress)) + (fadeToTextAlpha/2.25)
+	
+    text[4]	   = (text[4] * progress) + fadeToTextAlpha
+    outline[4] = (outline[4] * progress) + (fadeToTextAlpha/2.25)
 		
-    shaderAlpha = progress * maxShaderAlpha
-    nonShaderAlpha = progress * maxNonShaderAlpha
+    shaderAlpha = (1.0-progress) * maxShaderAlpha
+    nonShaderAlpha = (1.0-progress) * maxNonShaderAlpha
     
     glPushMatrix()
     
-	  if not shaderProgram then
-   		glColor(0,0,0,nonShaderAlpha)
-    	glRect( 0, 0, vsx, vsy)
-	  end
+	if not shaderProgram then
+   		glColor(0, 0, 0, nonShaderAlpha)
+    	glRect(0, 0, vsx, vsy)
+	end
 	  
-    glTranslate(-vsx*(usedSizeMultiplier-1)/2,  -vsy*(usedSizeMultiplier-1)/2, 0)
+    glTranslate(-vsx*(usedSizeMultiplier-1)/2, -vsy*(usedSizeMultiplier-1)/2, 0)
     glScale(usedSizeMultiplier,usedSizeMultiplier,1)
-    if ( diffPauseTime <= slideTime ) then
-        --we are sliding
-        if ( paused ) then
-            --sliding in
-            glTranslate( (( vsx - wndX1 ) / usedSizeMultiplier) * ( 1.0 - ( diffPauseTime / slideTime ) ), 0, 0)
-        else
-            --sliding out
-            glTranslate( (( vsx - wndX1 ) / usedSizeMultiplier) * ( ( diffPauseTime / slideTime ) ), 0, 0)
-        end
-    end
+	glTranslate((vsx-wndX1) / usedSizeMultiplier * progress, 0, 0)
     
     --draw text
     myFont:Begin()
-    myFont:SetOutlineColor( outline )
-    myFont:SetTextColor( text )
-    myFont:Print( "GAME  PAUSED", textX, textY, fontSizeHeadline, "O" )
+    myFont:SetOutlineColor(outline)
+    myFont:SetTextColor(text)
+    myFont:Print("GAME  PAUSED", textX, textY, fontSizeHeadline, "O")
     myFont:End()
     
     glPopMatrix()
@@ -273,8 +250,8 @@ function updateWindowCoords()
     wndX2 = (vsx / 2) + boxWidth
     wndY2 = (vsy / 2) - boxHeight
 
-    textX = wndX1 + ( wndX2 - wndX1 ) * 0.33
-    textY = wndY2 + ( wndY1 - wndY2 ) * 0.4
+    textX = wndX1 + (wndX2 - wndX1) * 0.33
+    textY = wndY2 + (wndY1 - wndY2) * 0.4
 end
 
 function widget:ViewResize(viewSizeX, viewSizeY)
@@ -291,14 +268,14 @@ function widget:ViewResize(viewSizeX, viewSizeY)
 end
 
 function widget:DrawScreenEffects()
-  if Spring.IsGUIHidden() then return end
-	if shaderProgram and showPauseScreen then
-		glCopyToTexture(screencopy, 0, 0, 0, 0, vsx, vsy)
-		glTexture(0, screencopy)
-		glUseShader(shaderProgram)
-		glUniform(alphaLoc, shaderAlpha)
-		glTexRect(0,vsy,vsx,0)
-		glTexture(0, false)
-		glUseShader(0)
-	end
+	if not shaderProgram then return end
+	if not drawPauseScreen then return end
+	
+	glCopyToTexture(screencopy, 0, 0, 0, 0, vsx, vsy)
+	glTexture(0, screencopy)
+	glUseShader(shaderProgram)
+	glUniform(alphaLoc, shaderAlpha)
+	glTexRect(0,vsy,vsx,0)
+	glTexture(0, false)
+	glUseShader(0)
 end
