b87218a2f241a6391a0cc57011091c6f571bf28f
MasterBel2 <untakenprefix@gmail.com>
2020-08-26 01:51:07 +1000

    Update default hotkeys
    (Old hotkeys are available as “BA Hotkeys old” widget)

--- luaui/widgets/cmd_BA_hotkeys.lua
+++ luaui/widgets/cmd_BA_hotkeys.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
 	return {
 		name = "BA Hotkeys",
-		desc = "Enables BA Hotkeys, including ZXCV,BN,YJ,O,Q" ,
+		desc =  "z mex mm, x enrgy, c turret, v nano rad jam, b aa, n anti sheild" ,
 		author = "Beherith",
 		date = "23 march 2012",
 		license = "GNU LGPL, v2.1 or later",
@@ -10,7 +10,9 @@ function widget:GetInfo()
         handler = true,
 	}
 end
-
+local minWind               = Game.windMin
+local maxWind               = Game.windMax
+local avgWind = math.floor((maxWind + minWind) / 2)
 
 -- table of stuff that we unbind on load
 local unbinds={
@@ -25,6 +27,9 @@ local unbinds={
 
     "bind , prevmenu",
     "bind . nextmenu",
+	
+	
+
 }
 
 -- table of stuff that we bind on load
@@ -37,113 +42,246 @@ function MakeBindsTable (swapYZ)
     
     local _binds = {
         -- building hotkeys
-        "bind "..Z.." buildunit_armmex",
-        "bind shift+"..Z.." buildunit_armmex",
-        "bind "..Z.." buildunit_armamex",
-        "bind shift+"..Z.." buildunit_armamex",
-        "bind "..Z.." buildunit_cormex",
-        "bind shift+"..Z.." buildunit_cormex",
-        "bind "..Z.." buildunit_corexp",
-        "bind shift+"..Z.." buildunit_corexp",
-        "bind "..Z.." buildunit_armmoho",
-        "bind shift+"..Z.." buildunit_armmoho",
-        "bind "..Z.." buildunit_cormoho",
-        "bind shift+"..Z.." buildunit_cormoho",
-        "bind "..Z.." buildunit_cormexp",
-        "bind shift+"..Z.." buildunit_cormexp",
-        "bind "..Z.." buildunit_coruwmex",
-        "bind shift+"..Z.." buildunit_coruwmex",
-        "bind "..Z.." buildunit_armuwmex",
-        "bind shift+"..Z.." buildunit_armuwmex",
-        "bind "..Z.." buildunit_coruwmme",
-        "bind shift+"..Z.." buildunit_coruwmme",
-        "bind "..Z.." buildunit_armuwmme",
-        "bind shift+"..Z.." buildunit_armuwmme",
-        "bind x buildunit_armsolar",
-        "bind shift+x buildunit_armsolar",
-        "bind x buildunit_armwin",
+		
+		--Z X C V B
+			--Z = mex and mm
+			--X = e
+			--C = turrets
+			--V = nano radar jammar 
+			--B = aa
+			--N = anti/sheild 
+			
+		--Z = mex and mm
+		
+		--t2 mex
+		"bind "..Z.."  buildunit_armmoho",
+        "bind shift+"..Z.."  buildunit_armmoho",
+        "bind "..Z.."  buildunit_cormoho",
+        "bind shift+"..Z.."  buildunit_cormoho",
+		--t2 uw
+		"bind "..Z.."  buildunit_coruwmme",
+        "bind shift+"..Z.."  buildunit_coruwmme",
+        "bind "..Z.."  buildunit_armuwmme",
+        "bind shift+"..Z.."  buildunit_armuwmme",
+		--t2 mm
+		"bind "..Z.." buildunit_cormmkr",
+        "bind shift+"..Z.." buildunit_cormmkr",
+        "bind "..Z.." buildunit_armmmkr",
+		"bind shift+"..Z.." buildunit_armmmkr",
+		--t2 mm w
+		"bind "..Z.." buildunit_armuwmmm",
+        "bind shift+"..Z.." buildunit_armuwmmm",
+        "bind "..Z.." buildunit_coruwmmm",
+        "bind shift+"..Z.." buildunit_coruwmmm",
+		--t1 mex
+		"bind "..Z.."  buildunit_armmex",
+        "bind shift+"..Z.."  buildunit_armmex",
+        "bind "..Z.."  buildunit_cormex",
+        "bind shift+"..Z.."  buildunit_cormex",
+        --t1 uw
+        "bind "..Z.."  buildunit_coruwmex",
+        "bind shift+"..Z.."  buildunit_coruwmex",
+        "bind "..Z.."  buildunit_armuwmex",
+        "bind shift+"..Z.."  buildunit_armuwmex",
+		--t1 mm
+        "bind "..Z.."  buildunit_armmakr",
+        "bind shift+"..Z.."  buildunit_armmakr",
+        "bind "..Z.."  buildunit_cormakr",
+        "bind shift+"..Z.."  buildunit_cormakr",
+		--t1 wmm
+        "bind "..Z.."  buildunit_armfmkr",
+        "bind shift+"..Z.."  buildunit_armfmkr",
+        "bind "..Z.."  buildunit_corfmkr",
+        "bind shift+"..Z.."  buildunit_corfmkr",
+		--X = e
+		
+		--afus
+		"bind x buildunit_aafus",
+        "bind shift+x buildunit_aafus",
+		"bind x buildunit_cafus",
+        "bind shift+x buildunit_cafus",
+		--fus
+		"bind x buildunit_armfus",
+        "bind shift+x buildunit_armfus",
+        "bind x buildunit_corfus",
+        "bind shift+x buildunit_corfus",
+		--fus w
+		"bind x buildunit_armuwfus",
+        "bind shift+x buildunit_armuwfus",
+        "bind x buildunit_coruwfus",
+        "bind shift+x buildunit_coruwfus",
+	}	
+		
+		
+		
+		local _binds2
+		if (avgWind > 7) then
+		--wind
+		_binds2 = {
+		"bind x buildunit_armwin",
         "bind shift+x buildunit_armwin",
-        "bind x buildunit_corsolar",
-        "bind shift+x buildunit_corsolar",
-        "bind x buildunit_corwin",
+		"bind x buildunit_corwin",
         "bind shift+x buildunit_corwin",
+		--adv solar
         "bind x buildunit_armadvsol",
         "bind shift+x buildunit_armadvsol",
         "bind x buildunit_coradvsol",
         "bind shift+x buildunit_coradvsol",
-        "bind x buildunit_armfus",
-        "bind shift+x buildunit_armfus",
-        "bind x buildunit_armmmkr",
-        "bind shift+x buildunit_armmmkr",
-        "bind x buildunit_corfus",
-        "bind shift+x buildunit_corfus",
-        "bind x buildunit_cormmkr",
-        "bind shift+x buildunit_cormmkr",
+		--solar
+		"bind x buildunit_armsolar",
+        "bind shift+x buildunit_armsolar",
+        "bind x buildunit_corsolar",
+        "bind shift+x buildunit_corsolar",
+		}
+		else
+		_binds2 = {
+		--solar
+		"bind x buildunit_armsolar",
+        "bind shift+x buildunit_armsolar",
+        "bind x buildunit_corsolar",
+        "bind shift+x buildunit_corsolar",
+		--adv solar
+        "bind x buildunit_armadvsol",
+        "bind shift+x buildunit_armadvsol",
+        "bind x buildunit_coradvsol",
+        "bind shift+x buildunit_coradvsol",
+		--wind
+		"bind x buildunit_armwin",
+        "bind shift+x buildunit_armwin",
+		"bind x buildunit_corwin",
+        "bind shift+x buildunit_corwin",
+		}
+		end
+		
+		
+		local _binds3 =  {
+		--tidal
         "bind x buildunit_armtide",
         "bind shift+x buildunit_armtide",
         "bind x buildunit_cortide",
         "bind shift+x buildunit_cortide",
-        "bind x buildunit_armuwfus",
-        "bind shift+x buildunit_armuwfus",
-        "bind x buildunit_coruwfus",
-        "bind shift+x buildunit_coruwfus",
-        "bind x buildunit_armuwmmm",
-        "bind shift+x buildunit_armuwmmm",
-        "bind x buildunit_coruwmmm",
-        "bind shift+x buildunit_coruwmmm",
-        "bind c buildunit_armllt",
-        "bind shift+c buildunit_armllt",
-        "bind c buildunit_armrad",
-        "bind shift+c buildunit_armrad",
-        "bind c buildunit_corllt",
-        "bind shift+c buildunit_corllt",
-        "bind c buildunit_corrad",
-        "bind shift+c buildunit_corrad",
-        "bind c buildunit_corrl",
-        "bind shift+c buildunit_corrl",
-        "bind c buildunit_armrl",
-        "bind shift+c buildunit_armrl",
-        "bind c buildunit_armpb",
+		
+		--C = turrets  
+		
+		
+		
+		--float mine
+		"bind c buildunit_corfmine3",
+        "bind shift+c buildunit_corfmine3",
+		"bind c buildunit_armfmine3",
+        "bind shift+c buildunit_armfmine3",
+		--h mine
+		"bind c buildunit_cormine3",
+        "bind shift+c buildunit_cormine3",
+		"bind c buildunit_armmine3",
+        "bind shift+c buildunit_armmine3",
+		--m mine
+		"bind c buildunit_cormine2",
+        "bind shift+c buildunit_cormine2",
+		"bind c buildunit_armmine2",
+        "bind shift+c buildunit_armmine2",
+
+		
+		--t2 turret
+		"bind c buildunit_armpb",
         "bind shift+c buildunit_armpb",
-        "bind c buildunit_armflak",
-        "bind shift+c buildunit_armflak",
-        "bind c buildunit_corvipe",
+		"bind c buildunit_corvipe",
         "bind shift+c buildunit_corvipe",
-        "bind c buildunit_corflak",
-        "bind shift+c buildunit_corflak",
-        "bind c buildunit_armtl",
-        "bind shift+c buildunit_armtl",
-        "bind c buildunit_cortl",
-        "bind shift+c buildunit_cortl",
-        "bind c buildunit_armsonar",
-        "bind shift+c buildunit_armsonar",
-        "bind c buildunit_corsonar",
-        "bind shift+c buildunit_corsonar",
-        "bind c buildunit_armfrad",
-        "bind shift+c buildunit_armfrad",
-        "bind c buildunit_corfrad",
-        "bind shift+c buildunit_corfrad",
-        "bind c buildunit_armfrt",
-        "bind shift+c buildunit_armfrt",
-        "bind c buildunit_corfrt",
-        "bind shift+c buildunit_corfrt",
-        "bind v buildunit_armnanotc",
+		--doom anni
+		"bind c buildunit_cordoom",
+        "bind shift+c buildunit_cordoom",
+		"bind c buildunit_armanni",
+        "bind shift+c buildunit_armanni",
+		--llt
+		"bind c buildunit_armllt",
+		"bind shift+c buildunit_armllt",
+		"bind c buildunit_corllt",
+        "bind shift+c buildunit_corllt",
+		--hllt
+		"bind c buildunit_hllt",
+		"bind shift+c buildunit_hllt",
+		"bind c buildunit_tawf001",
+        "bind shift+c buildunit_tawf001",
+		--claw maw
+		"bind c buildunit_armclaw",
+		"bind shift+c buildunit_armclaw",
+		"bind c buildunit_cormaw",
+        "bind shift+c buildunit_cormaw",	
+		
+		
+				
+		
+		--V = nano radar jammar 
+		
+		--nano
+		"bind v buildunit_armnanotc",
         "bind shift+v buildunit_armnanotc",
-        "bind v buildunit_armlab",
-        "bind shift+v buildunit_armlab",
-        "bind v buildunit_armvp",
-        "bind shift+v buildunit_armvp",
-        "bind v buildunit_cornanotc",
-        "bind shift+v buildunit_cornanotc",
-        "bind v buildunit_corlab",
-        "bind shift+v buildunit_corlab",
-        "bind v buildunit_corvp",
-        "bind shift+v buildunit_corvp",
-        "bind v buildunit_armsy",
-        "bind shift+v buildunit_armsy",
-        "bind v buildunit_corsy",
-        "bind shift+v buildunit_corsy",
-        
+		"bind v buildunit_cornanotc",
+        "bind shift+v buildunit_cornanotc",	
+		--t2 jam
+		"bind v buildunit_corshroud",
+        "bind shift+v buildunit_corshroud",
+        "bind v buildunit_armveil",
+        "bind shift+v buildunit_armveil",
+		--t2 rad
+		"bind v buildunit_armarad",
+        "bind shift+v buildunit_armarad",
+        "bind v buildunit_corarad",
+        "bind shift+v buildunit_corarad",
+		--rad
+		"bind v buildunit_armrad",
+        "bind shift+v buildunit_armrad",
+        "bind v buildunit_corrad",
+        "bind shift+v buildunit_corrad",
+		--rad w
+        "bind v buildunit_armfrad",
+        "bind shift+v buildunit_armfrad",
+        "bind v buildunit_corfrad",
+        "bind shift+v buildunit_corfrad",
+		
+		--B = aa
+		
+		--flak
+		"bind b buildunit_armflak",
+        "bind shift+b buildunit_armflak",
+		"bind b buildunit_corflak",
+        "bind shift+b buildunit_corflak",
+		--flak w
+		"bind b buildunit_corenaa",
+        "bind shift+b buildunit_corenaa",
+		"bind b buildunit_armfflak",
+        "bind shift+b buildunit_armfflak",
+		--pako sucks only sam
+		"bind b buildunit_madsam",
+        "bind shift+b buildunit_madsam",
+		--merc scream
+		"bind b buildunit_mercury",
+        "bind shift+b buildunit_mercury",
+		"bind b buildunit_screamer",
+        "bind shift+b buildunit_screamer",
+		--t1 aa
+        "bind b buildunit_corrl",
+        "bind shift+b buildunit_corrl",
+        "bind b buildunit_armrl",
+        "bind shift+b buildunit_armrl",
+		--aa w
+		"bind b buildunit_armfrt",
+        "bind shift+b buildunit_armfrt",
+        "bind b buildunit_corfrt",
+        "bind shift+b buildunit_corfrt",
+		
+		--N = anti/sheild 
+		--anti
+		"bind n buildunit_corfmd",
+        "bind shift+n buildunit_corfmd",
+        "bind n buildunit_armamd",
+        "bind shift+n buildunit_armamd",
+		--sheild
+		"bind n buildunit_corgate",
+        "bind shift+n buildunit_corgate",
+        "bind n buildunit_armgate",
+        "bind shift+n buildunit_armgate",
+ 
         -- build spacing
         "bind any+b buildspacing inc",
         "bind any+n buildspacing dec",    
@@ -158,28 +296,32 @@ function MakeBindsTable (swapYZ)
         "bind numpad1 movefast",
         
         -- set target
-        "bind "..Y.." settargetnoground",
-        "bind alt+"..Y.." settarget",
+        "bind q settarget",
         "bind j canceltarget",
         
-        "bind q drawinmap", --some keyboards don't have ` or \
+        "bind * drawinmap", --some keyboards don't have ` or \
         "bind ,	buildfacing inc", --because some keyboards don't have [ and ] ke"..Y.."s
         "bind .	buildfacing dec",
         "bind o buildfacing inc", --apparently some keyboards don't have , and . either...
-
-    }
+        
+        "bind ,	buildfacing inc", --because some keyboards don't have [ and ] ke"..Y.."s
+        "bind .	buildfacing dec",
+        "bind o buildfacing inc", --apparently some keyboards don't have , and . either...
+		}
+   
     
-    binds = _binds
+	binds = table.merge(_binds,_binds2)
+	binds = table.merge(binds,_binds3)
 end
-
------------
 
 function LoadBindings()
 	for k,v in ipairs(unbinds) do
 		Spring.SendCommands("un"..v)
 	end
     
-    MakeBindsTable(WG.swapYZbinds) -- in case Y/Z swap has changed since last load
+
+		MakeBindsTable(WG.swapYZbinds)   
+
     
 	for k,v in ipairs(binds) do
 		Spring.SendCommands(v)
@@ -194,6 +336,14 @@ function UnloadBindings()
 	for k,v in ipairs(unbinds) do
 		Spring.SendCommands(v)
 	end
+end
+
+function table.merge(t1, t2)
+   for k,v in ipairs(t2) do
+      table.insert(t1, v)
+   end 
+ 
+   return t1
 end
 
 function ReloadBindings()
