34fbf1846d487d1489c287dc284ad96a79d6760a
MasterBel2 <untakenprefix@gmail.com>
2020-08-26 01:57:33 +1000

    Add default spring settings

---
+++ luaui/widgets/springsettings_defaults.lua
@@ -0,0 +1,90 @@
+
+function widget:GetInfo()
+	return {
+		name      = "springsettings defaults",
+		desc      = "default settings",
+		author    = "Niobium",
+		date      = "3 April 2010",
+		license   = "GNU GPL, v2 or later",
+		layer     = 0,
+		enabled   = true
+	}
+end
+
+local minMaxparticles = 3000
+
+local function reducePing()
+	Spring.SendCommands("UseNetMessageSmoothingBuffer ".."0")
+	Spring.SendCommands("NetworkLossFactor ".."2")
+	Spring.SendCommands("LinkOutgoingBandwidth ".."262144")
+	Spring.SendCommands("LinkIncomingSustainedBandiwdth ".."262144")
+	Spring.SendCommands("LinkIncomingPeakBandiwdth ".."262144")
+	Spring.SendCommands("LinkIncomingMaxPacketRate ".."2048")
+	
+	Spring.SetConfigString("UseNetMessageSmoothingBuffer", '0')
+		Spring.SetConfigString("NetworkLossFactor", '2')
+		Spring.SetConfigString("LinkOutgoingBandwidth", '262144')
+		Spring.SetConfigString("LinkIncomingSustainedBandwidth", '262144')
+		Spring.SetConfigString("LinkIncomingPeakBandwidth", '262144')
+		Spring.SetConfigString("LinkIncomingMaxPacketRate", '2048')
+		
+		Spring.SendCommands("FeatureDrawDistance ".."100000")
+		Spring.SendCommands("FeatureFadeDistance ".."100000")
+		Spring.SetConfigString("FeatureDrawDistance", '100000')
+		Spring.SetConfigString("FeatureFadeDistance", '100000') 
+end
+
+local function setEngineFont()
+local font = "LuaUI/Fonts/FreeSansBold.otf"
+		Spring.SendCommands("font "..font)
+		Spring.SendCommands("font ".."")
+		Spring.SetConfigString("SmallFontFile", 'FreeSansBold.otf')
+		Spring.SetConfigString("FontFile", 'FreeSansBold.otf')
+end
+
+local firstlaunchsetupDone = false
+function widget:GetConfigData()
+    savedTable = {}
+    savedTable.firsttimesetupDone = firstlaunchsetupDone
+    return savedTable
+end
+
+function widget:SetConfigData(data)
+    if data.firsttimesetupDone ~= nil then
+        firstlaunchsetupDone = data.firsttimesetupDone
+    end
+end
+
+function widget:Initialize()
+
+					Spring.SendCommands("UnitLodDist".."1000") --need this to remove it from springsettings
+					Spring.SetConfigString("UnitLodDist", '1000')
+					Spring.SetConfigString("hangtimeout", '-10')
+
+   reducePing()
+   
+   if firstlaunchsetupDone == false then
+		
+			
+		 if tonumber(Spring.GetConfigInt("MaxParticles",1) or 0) < minMaxparticles then
+            Spring.SendCommands("MaxParticles".. minMaxparticles)
+			Spring.SetConfigInt("MaxParticles", minMaxparticles)
+        end
+
+		Spring.SendCommands("clock".. "1")
+		Spring.SetConfigInt("clock",1)
+		
+		Spring.SendCommands("Vsync".."0")
+		Spring.SetConfigInt("Vsync",0)
+		
+		Spring.SendCommands("MiniMapDrawProjectiles",0)
+		Spring.SetConfigInt("MiniMapDrawProjectiles",0)
+
+        firstlaunchsetupDone = true
+    end
+   
+   setEngineFont()
+   
+    widgetHandler:RemoveWidget(self)
+end
+
