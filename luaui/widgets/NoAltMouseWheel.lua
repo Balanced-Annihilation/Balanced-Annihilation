a791427e003874b0bfd7f848dffc51599ef15e18
MasterBel2 <untakenprefix@gmail.com>
2020-08-26 01:58:23 +1000

    Disabling mouse wheel

---
+++ luaui/widgets/NoAltMouseWheel.lua
@@ -0,0 +1,21 @@
+function widget:GetInfo()
+	return {
+		name = "No Alt Mouse Wheel",
+		desc = "Eats all your mousewheeling when alt is down",
+		author = "zwzsg",
+		date = "16 may 2010",
+		license = "Free",
+		version = 1,
+		layer = 2000, -- so that any widget with a lower number has a chance to catch mousewheel first
+		enabled = true
+	}
+end
+
+function widget:MouseWheel(up,value)
+	local alt,ctrl,meta,shift=Spring.GetModKeyState()
+	if alt then
+		return true
+	else
+		return false
+	end
+end
