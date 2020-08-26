a9ee6537bf7fa0ca3d18e48623fa265194382870
MasterBel2 <untakenprefix@gmail.com>
2020-08-26 18:23:59 +1000

    Update springsettings defaults
    - Some formatting changes
    - Remove duplicate font commands
    - Add profanity filter mod option
    - Advanced sky defaults to off, since it can cause crashes
    - Default vsync to off
    - Default ground detail to 100

--- luaui/widgets/set_springsettings_defaults.lua
+++ luaui/widgets/set_springsettings_defaults.lua
@@ -7,11 +7,12 @@ function widget:GetInfo()
 		date      = "3 April 2010",
 		license   = "GNU GPL, v2 or later",
 		layer     = 0,
+		handler = true,
 		enabled   = true
 	}
 end
 
-local minMaxparticles = 8000
+local minMaxparticles = 15000
 
 local function reducePing()
 	Spring.SendCommands("UseNetMessageSmoothingBuffer ".."0")
@@ -21,12 +22,12 @@ local function reducePing()
 	Spring.SendCommands("LinkIncomingPeakBandiwdth ".."262144")
 	Spring.SendCommands("LinkIncomingMaxPacketRate ".."2048")
 	
-	Spring.SetConfigString("UseNetMessageSmoothingBuffer ", '0')
-		Spring.SetConfigString("NetworkLossFactor ", '2')
-		Spring.SetConfigString("LinkOutgoingBandwidth ", '262144')
-		Spring.SetConfigString("LinkIncomingSustainedBandwidth ", '262144')
-		Spring.SetConfigString("LinkIncomingPeakBandwidth ", '262144')
-		Spring.SetConfigString("LinkIncomingMaxPacketRate ", '2048')
+	Spring.SetConfigString("UseNetMessageSmoothingBuffer", '0')
+		Spring.SetConfigString("NetworkLossFactor", '2')
+		Spring.SetConfigString("LinkOutgoingBandwidth", '262144')
+		Spring.SetConfigString("LinkIncomingSustainedBandwidth", '262144')
+		Spring.SetConfigString("LinkIncomingPeakBandwidth", '262144')
+		Spring.SetConfigString("LinkIncomingMaxPacketRate", '2048')
 		
 		--Spring.SendCommands("FeatureDrawDistance ".."90000")
 		--Spring.SendCommands("FeatureFadeDistance ".."90000")
@@ -44,14 +45,9 @@ local function setEngineFont()
 		Spring.SetConfigString('SmallFontSize','14')
 		
 		Spring.SendCommands("font "..font)
-		Spring.SendCommands("font ".."")
+		--Spring.SendCommands("font ".."")
 		Spring.SetConfigString("SmallFontFile", 'FreeSansBold.otf')
 		Spring.SetConfigString("FontFile", 'FreeSansBold.otf')
-		
-		
-
-
-		
 end
 
 local firstlaunchsetupDone = false
@@ -69,6 +65,13 @@ end
 
 function widget:Initialize()
 
+
+		
+
+		local camState = Spring.GetCameraState()
+		camState.mode = 1
+		Spring.SetCameraState(camState,0)
+
 					--Spring.SendCommands("UnitLodDist ".."90000") --need this to remove it from springsettings
 					Spring.SetConfigString("UnitLodDist", '90000')
 					Spring.SetConfigString("HangTimeout", '-10')
@@ -77,30 +80,33 @@ function widget:Initialize()
 	
    
    if firstlaunchsetupDone == false then
+	Spring.SetConfigString("UseNetMessageSmoothingBuffer", '0')
+   		Spring.SetConfigString("VSync", '0')
 		Spring.SetConfigString("Water", '1')
-		Spring.SetConfigString("AdvSky", '1')
+		Spring.SetConfigString("ProfanityFilter", '1')
+		Spring.SetConfigString("AdvSky", '0') --1 would crash tassles
 		Spring.SetConfigString("reduceping", '1')
 		reducePing()
 		Spring.SetConfigString("LuaShaders", '1')
 		Spring.SetConfigString("FeatureDrawDistance", '90000')
 		Spring.SetConfigString("FeatureFadeDistance", '90000') 
-		
-		 if tonumber(Spring.GetConfigInt("MaxParticles",1) or 0) < minMaxparticles then
-            Spring.SendCommands("MaxParticles ".. minMaxparticles)
-			Spring.SetConfigInt("MaxParticles", minMaxparticles)
-        end
 		Spring.SetConfigString("GrassDetail",'0')
-
 		Spring.SetConfigString("MiniMapDrawProjectiles", '0')
-
-        firstlaunchsetupDone = true
+		firstlaunchsetupDone = true
+		
+        Spring.SendCommands("MaxParticles ".. minMaxparticles)
+		Spring.SetConfigInt("MaxParticles", minMaxparticles)
+		Spring.SetConfigInt("GroundDetail", 100)    
     end
 	
 	if tonumber(Spring.GetConfigInt("reduceping",1) or 1) == 1 then
    reducePing()
    end
    
+
+   
    setEngineFont()
+	
    
     widgetHandler:RemoveWidget(self)
 end
