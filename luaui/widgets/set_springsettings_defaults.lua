c7733ade0ad18543d253a1b65d632ff4b3d75114
MasterBel2 <untakenprefix@gmail.com>
2020-08-26 16:30:03 +1000

    Default Springsetings update & rename
    Now “set_…”
    Doesn’t send commands for Feature draw distance, but updated value from 10000->90000
    “” for UnitLODDistance
    Update font style
    Default LuaShaders to 1

--- luaui/widgets/set_springsettings_defaults.lua
+++ luaui/widgets/set_springsettings_defaults.lua
@@ -1,7 +1,7 @@
 
 function widget:GetInfo()
 	return {
-		name      = "springsettings defaults",
+		name      = "set springsettings defaults",
 		desc      = "default settings",
 		author    = "Niobium",
 		date      = "3 April 2010",
@@ -11,7 +11,7 @@ function widget:GetInfo()
 	}
 end
 
-local minMaxparticles = 5000
+local minMaxparticles = 7000
 
 local function reducePing()
 	Spring.SendCommands("UseNetMessageSmoothingBuffer ".."0")
@@ -28,18 +28,31 @@ local function reducePing()
 		Spring.SetConfigString("LinkIncomingPeakBandwidth ", '262144')
 		Spring.SetConfigString("LinkIncomingMaxPacketRate ", '2048')
 		
-		Spring.SendCommands("FeatureDrawDistance ".."10000")
-		Spring.SendCommands("FeatureFadeDistance ".."10000")
-		Spring.SetConfigString("FeatureDrawDistance", '10000')
-		Spring.SetConfigString("FeatureFadeDistance", '10000') 
+		--Spring.SendCommands("FeatureDrawDistance ".."90000")
+		--Spring.SendCommands("FeatureFadeDistance ".."90000")
+		Spring.SetConfigString("FeatureDrawDistance", '90000')
+		Spring.SetConfigString("FeatureFadeDistance", '90000') 
 end
 
 local function setEngineFont()
-local font = "LuaUI/Fonts/FreeSansBold.otf"
+		local font = "FreeSansBold.otf"
+		
+		Spring.SetConfigString('FontOutlineWeight','25')
+		Spring.SetConfigString('FontOutlineWidth','3')
+		Spring.SetConfigString('FontSize','23')
+		Spring.SetConfigString('SmallFontOutlineWeight','10')
+		Spring.SetConfigString('SmallFontOutlineWidth','2')
+		Spring.SetConfigString('SmallFontSize','14')
+		
 		Spring.SendCommands("font "..font)
 		Spring.SendCommands("font ".."")
 		Spring.SetConfigString("SmallFontFile", 'FreeSansBold.otf')
 		Spring.SetConfigString("FontFile", 'FreeSansBold.otf')
+		
+		
+
+
+		
 end
 
 local firstlaunchsetupDone = false
@@ -57,15 +70,16 @@ end
 
 function widget:Initialize()
 
-					Spring.SendCommands("UnitLodDist ".."10000") --need this to remove it from springsettings
-					Spring.SetConfigString("UnitLodDist", '10000')
+					--Spring.SendCommands("UnitLodDist ".."90000") --need this to remove it from springsettings
+					Spring.SetConfigString("UnitLodDist", '90000')
 					Spring.SetConfigString("hangtimeout", '0')
 
    reducePing()
    
    if firstlaunchsetupDone == false then
 		
-			
+		Spring.SetConfigInt("LuaShaders", 1)
+
 		 if tonumber(Spring.GetConfigInt("MaxParticles",1) or 0) < minMaxparticles then
             Spring.SendCommands("MaxParticles ".. minMaxparticles)
 			Spring.SetConfigInt("MaxParticles", minMaxparticles)
