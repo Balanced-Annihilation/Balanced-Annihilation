a53ddd89f95fa10960092a3e04a774616d12e5a2
MasterBel2 <untakenprefix@gmail.com>
2020-08-26 16:00:45 +1000

    Buff loadscreen

--- luaintro/Addons/main.lua
+++ luaintro/Addons/main.lua
@@ -14,6 +14,9 @@ end
 
 ------------------------------------------
 
+local showTips = false
+
+
 local lastLoadMessage = ""
 
 function addon.LoadProgress(message, replaceLastLine)
@@ -22,162 +25,219 @@ end
 
 ------------------------------------------
 
-local font = gl.LoadFont("FreeSansBold.otf", 50, 20, 1.95)
+
+
+local titleColor = "\255\215\255\215"
+local contentColor = "\255\255\255\255"
+
+
+-- Since math.random is not random and always the same, we save a counter to a file and use that.
+filename = "LuaUI/Config/randomseed.data"
+k = os.time() % 1500
+if VFS.FileExists(filename) then
+	k = tonumber(VFS.LoadFile(filename))
+	if not k then k = 0 end
+end
+k = k + 1
+local file = assert(io.open(filename,'w'), "Unable to save latest randomseed from "..filename)
+    file:write(k)
+    file:close()
+file = nil
+
+
+local loadedFontSize = 70
+local font = gl.LoadFont("FreeSansBold.otf", 70, 22, 1.15)
+
+function DrawRectRound(px,py,sx,sy,cs)
+
+	--local csx = cs
+	--local csy = cs
+	--if sx-px < (cs*2) then
+	--	csx = (sx-px)/2
+	--	if csx < 0 then csx = 0 end
+	--end
+	--if sy-py < (cs*2) then
+	--	csy = (sy-py)/2
+	--	if csy < 0 then csy = 0 end
+	--end
+	--cs = math.min(cs, csy)
+
+	gl.TexCoord(0.8,0.8)
+	gl.Vertex(px+cs, py, 0)
+	gl.Vertex(sx-cs, py, 0)
+	gl.Vertex(sx-cs, sy, 0)
+	gl.Vertex(px+cs, sy, 0)
+
+	gl.Vertex(px, py+cs, 0)
+	gl.Vertex(px+cs, py+cs, 0)
+	gl.Vertex(px+cs, sy-cs, 0)
+	gl.Vertex(px, sy-cs, 0)
+	
+	gl.Vertex(sx, py+cs, 0)
+	gl.Vertex(sx-cs, py+cs, 0)
+	gl.Vertex(sx-cs, sy-cs, 0)
+	gl.Vertex(sx, sy-cs, 0)
+	
+	local offset = 0.05		-- texture offset, because else gaps could show
+	local o = offset
+	
+	-- top left
+	--if py <= 0 or px <= 0 then o = 0.5 else o = offset end
+	gl.TexCoord(o,o)
+	gl.Vertex(px, py, 0)
+	gl.TexCoord(o,1-o)
+	gl.Vertex(px+cs, py, 0)
+	gl.TexCoord(1-o,1-o)
+	gl.Vertex(px+cs, py+cs, 0)
+	gl.TexCoord(1-o,o)
+	gl.Vertex(px, py+cs, 0)
+	-- top right
+	--if py <= 0 or sx >= vsx then o = 0.5 else o = offset end
+	gl.TexCoord(o,o)
+	gl.Vertex(sx, py, 0)
+	gl.TexCoord(o,1-o)
+	gl.Vertex(sx-cs, py, 0)
+	gl.TexCoord(1-o,1-o)
+	gl.Vertex(sx-cs, py+cs, 0)
+	gl.TexCoord(1-o,o)
+	gl.Vertex(sx, py+cs, 0)
+	-- bottom left
+	--if sy >= vsy or px <= 0 then o = 0.5 else o = offset end
+	gl.TexCoord(o,o)
+	gl.Vertex(px, sy, 0)
+	gl.TexCoord(o,1-o)
+	gl.Vertex(px+cs, sy, 0)
+	gl.TexCoord(1-o,1-o)
+	gl.Vertex(px+cs, sy-cs, 0)
+	gl.TexCoord(1-o,o)
+	gl.Vertex(px, sy-cs, 0)
+	-- bottom right
+	--if sy >= vsy or sx >= vsx then o = 0.5 else o = offset end
+	gl.TexCoord(o,o)
+	gl.Vertex(sx, sy, 0)
+	gl.TexCoord(o,1-o)
+	gl.Vertex(sx-cs, sy, 0)
+	gl.TexCoord(1-o,1-o)
+	gl.Vertex(sx-cs, sy-cs, 0)
+	gl.TexCoord(1-o,o)
+	gl.Vertex(sx, sy-cs, 0)
+end
+
+function RectRound(px,py,sx,sy,cs)
+	--local px,py,sx,sy,cs = math.floor(px),math.floor(py),math.ceil(sx),math.ceil(sy),math.floor(cs)
+	
+	gl.Texture(":n:luaui/Images/bgcorner.png")
+	--gl.Texture(":n:luaui/Images/bgcorner.png")
+	gl.BeginEnd(GL.QUADS, DrawRectRound, px,py,sx,sy,cs)
+	gl.Texture(false)
+end
+
+function gradienth(px,py,sx,sy, c1,c2)
+	gl.Color(c1)
+	gl.Vertex(sx, sy, 0)
+	gl.Vertex(sx, py, 0)
+	gl.Color(c2)
+	gl.Vertex(px, py, 0)
+	gl.Vertex(px, sy, 0)
+end
+
+
+local lastLoadMessage = ""
+local lastProgress = {0, 0}
+
+local progressByLastLine = {
+	["Parsing Map Information"] = {0, 20},
+	["Loading Weapon Definitions"] = {10, 50},
+	["Loading LuaRules"] = {40, 80},
+	["Loading LuaUI"] = {70, 95},
+	["Finalizing"] = {100, 100}
+}
+for name,val in pairs(progressByLastLine) do
+	progressByLastLine[name] = {val[1]*0.01, val[2]*0.01}
+end
+
+function addon.LoadProgress(message, replaceLastLine)
+	lastLoadMessage = message
+	if message:find("Path") then -- pathing has no rigid messages so cant use the table
+		lastProgress = {0.8, 1.0}
+	end
+	lastProgress = progressByLastLine[message] or lastProgress
+end
 
 function addon.DrawLoadScreen()
 	local loadProgress = SG.GetLoadProgress()
+	if loadProgress == 0 then
+		loadProgress = lastProgress[1]
+	else
+		loadProgress = math.min(math.max(loadProgress, lastProgress[1]), lastProgress[2])
+	end
 
 	local vsx, vsy = gl.GetViewSizes()
 
-
-	
 	-- draw progressbar
 	local hbw = 3.5/vsx
 	local vbw = 3.5/vsy
-	local hsw = 0.2
-	local vsw = 0.2
-	
-	gl.PushMatrix()
-	gl.Scale(.4,.4,1)
-	gl.Translate(-0.1,0,0)
-	
-	gl.BeginEnd(GL.QUADS, function()
-		--shadow topleft
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2-hsw, 0.2    )
-			gl.Vertex(0.2-hsw, 0.2+vsw)
-			gl.Vertex(0.2    , 0.2+vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.2    , 0.2)
+	local hsw = 0.4
+	local vsw = 0.4
+	local yPos =  0.3 --0.054
+	local yPosTips = yPos + 0.1245
+	local loadvalue = 0.2 + (math.max(0, loadProgress) * 0.6)
 
-		--shadow top
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2, 0.2+vsw)
-			gl.Vertex(0.8, 0.2+vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.2, 0.2)
+	if not showTips then
+		yPos = 0.165
+		yPosTips = yPos
+	end
 
-		--shadow topright
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.8    , 0.2+vsw)
-			gl.Vertex(0.8+hsw, 0.2+vsw)
-			gl.Vertex(0.8+hsw, 0.2)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8    , 0.2)
+	--bar bg
+	local paddingH = 0.004
+	local paddingW = paddingH * (vsy/vsx)
+	gl.Color(0.085,0.085,0.085,0.925)
+	RectRound(0.2-paddingW,yPos-0.05-paddingH,0.8+paddingW,yPosTips+paddingH,0.007)
 
-		--shadow right
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.8+hsw, 0.2)
-			gl.Vertex(0.8+hsw, 0.15)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8    , 0.15)
-			gl.Vertex(0.8    , 0.2)
+	gl.Color(0,0,0,0.75)
+	RectRound(0.2-paddingW,yPos-0.05-paddingH,0.8+paddingW,yPos+paddingH,0.007)
 
-		--shadow btmright
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.8    , 0.15-vsw)
-			gl.Vertex(0.8+hsw, 0.15-vsw)
-			gl.Vertex(0.8+hsw, 0.15)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8    , 0.15)
+    if loadvalue > 0.215 then
+	    -- loadvalue
+        gl.Color(0.4-(loadProgress/7),loadProgress*0.4,0,0.4)
+        RectRound(0.2,yPos-0.05,loadvalue,yPos,0.0055)
 
-		--shadow btm
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2, 0.15-vsw)
-			gl.Vertex(0.8, 0.15-vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.2, 0.15)
+        -- loadvalue gradient
+        gl.Texture(false)
+        gl.BeginEnd(GL.QUADS, gradienth, 0.2,yPos-0.05,loadvalue,yPos, {1-(loadProgress/3)+0.2,loadProgress+0.2,0+0.08,0.14}, {0,0,0,0.14})
 
-		--shadow btmleft
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2-hsw, 0.15    )
-			gl.Vertex(0.2-hsw, 0.15-vsw)
-			gl.Vertex(0.2    , 0.15-vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.2    , 0.15)
+        -- loadvalue inner glow
+        gl.Color(1-(loadProgress/3.5)+0.15,loadProgress+0.15,0+0.05,0.04)
+        gl.Texture(":n:luaui/Images/barglow-center.png")
+        gl.TexRect(0.2,yPos-0.05,loadvalue,yPos)
 
-		--shadow left
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2-hsw, 0.2)
-			gl.Vertex(0.2-hsw, 0.15)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.2    , 0.15)
-			gl.Vertex(0.2    , 0.2)
+        -- loadvalue glow
+        local glowSize = 0.06
+        gl.Color(1-(loadProgress/3)+0.15,loadProgress+0.15,0+0.05,0.1)
+        gl.Texture(":n:luaui/Images/barglow-center.png")
+        gl.TexRect(0.2,	yPos-0.05-glowSize,	loadvalue,	yPos+glowSize)
 
-		--bar bg
-		gl.Color(0,0,0,0.85)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.2, 0.15)
-
-		--progress
-		gl.Color(1,1,1,0.7)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.2)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.15)
-			gl.Vertex(0.2, 0.15)
-		gl.Color(1,1,1,0.7)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.2)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.15)
-		gl.Color(1,1,1,0)
-			gl.Vertex(0.2 + math.min(1, math.max(0, loadProgress+0.01)) * 0.6, 0.15)
-			gl.Vertex(0.2 + math.min(1, math.max(0, loadProgress+0.01)) * 0.6, 0.2)
-
-		--bar borders
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.8, 0.2-vbw)
-			gl.Vertex(0.2, 0.2-vbw)
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.2, 0.15)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.8, 0.15+vbw)
-			gl.Vertex(0.2, 0.15+vbw)
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.2, 0.15)
-			gl.Vertex(0.2+hbw, 0.15)
-			gl.Vertex(0.2+hbw, 0.2)
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.8-hbw, 0.15)
-			gl.Vertex(0.8-hbw, 0.2)
-	end)
-
---[[
-	gl.Color(0,0,0,1)
-	gl.Rect(0.2,0.15,0.8,0.2)
-	gl.Color(1,1,1,1)
-	gl.Rect(0.2,0.15,0.2 + math.max(0, loadProgress) * 0.6,0.2)
-	gl.LineWidth(5)
-	gl.PolygonMode(GL.FRONT_AND_BACK, GL.LINE)
-	gl.Rect(0.2,0.15,0.8,0.2)
-	gl.PolygonMode(GL.FRONT_AND_BACK, GL.FILL)
-	gl.LineWidth(1)
-	gl.Color(1,1,1,1)
---]]
+        gl.Texture(":n:luaui/Images/barglow-edge.png")
+        gl.TexRect(0.2-(glowSize*1.3), yPos-0.05-glowSize, 0.2, yPos+glowSize)
+        gl.TexRect(loadvalue+(glowSize*1.3), yPos-0.05-glowSize, loadvalue, yPos+glowSize)
+    end
 
 	-- progressbar text
 	gl.PushMatrix()
-	gl.Scale(1/vsx,1/vsy,1)
-		local barTextSize = vsy * (0.05 - 0.015)
+		gl.Scale(1/vsx,1/vsy,1)
+		local barTextSize = vsy * 0.026
 
 		--font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.3, 50, "sc")
 		--font:Print(Game.gameName, vsx * 0.5, vsy * 0.95, vsy * 0.07, "sca")
-		font:Print(lastLoadMessage, vsx * 0.2, vsy * 0.14, barTextSize, "sa")
+		font:Print(lastLoadMessage, vsx * 0.21, vsy * (yPos-0.017), barTextSize * 0.67, "oa")
 		if loadProgress>0 then
-			font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.165, barTextSize, "oc")
+			font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * (yPos-0.0325), barTextSize, "oc")
 		else
-			font:Print("Loading...", vsx * 0.5, vsy * 0.165, barTextSize, "oc")
+			font:Print("Loading...", vsx * 0.5, vsy * (yPos-0.031), barTextSize, "oc")
 		end
 	gl.PopMatrix()
-	
-	gl.PopMatrix()
+
+
 end
 
 
