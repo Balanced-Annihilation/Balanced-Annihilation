745cf88ab20543978239066911c4b5b18f52dfb4
MasterBel2 <untakenprefix@gmail.com>
2020-08-26 16:18:15 +1000

    Revert changes to unit_prevent_share_self_d
    From commit “Various UI Tweaks”

--- luarules/gadgets/unit_prevent_share_self_d.lua
+++ luarules/gadgets/unit_prevent_share_self_d.lua
@@ -1,48 +1,50 @@
 function gadget:GetInfo()
-	return {
-		name      = "No Share Self-D",
-		desc      = "Prevents self-destruction when a unit changes hands or a player leaves",
-		author    = "quantum, Bluestone",
-		date      = "July 13, 2008",
-		license   = "GNU GPL, v2 or later",
-		layer     = 0,
-		enabled   = true  --  loaded by default?
-	}
+  return {
+    name      = "No Share Self-D",
+    desc      = "Prevents self-destruction when a unit changes hands or a player leaves",
+    author    = "quantum, Bluestone",
+    date      = "July 13, 2008",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
 end
 
 
-if not gadgetHandler:IsSyncedCode() then
-
+if (not gadgetHandler:IsSyncedCode()) then
+ 
 function gadget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
   -- remove self d commands on shared units
-  if Spring.GetUnitSelfDTime(unitID) > 0 then
-    Spring.GiveOrderToUnit(unitID, CMD.SELFD, {}, 0)
+  if (Spring.GetUnitSelfDTime(unitID) > 0) then
+    Spring.GiveOrderToUnit(unitID, CMD.SELFD, {}, {})
   end
+end
+
+function gadget:PlayerChanged(playerID) --necessary wtfness, probably can remove in 97.0+
 end
 
 else
 
 -- remove self d orders from empty teams
 function gadget:PlayerChanged(playerID)
-	local _,active,spec,teamID = Spring.GetPlayerInfo(playerID,false)
+	local _,active,spec,teamID = Spring.GetPlayerInfo(playerID)
 	if active and not spec then return end
 	local team = Spring.GetPlayerList(teamID)
-
+	
 	if team then
 		-- check team is empty
 		for _,pID in pairs(team) do
-			_,active,spec = Spring.GetPlayerInfo(pID,false)
+			_,active,spec = Spring.GetPlayerInfo(pID)
 			if active and not spec then
 				return
 			end
 		end
-
+	
 		-- cancel any self d orders
 		local units = Spring.GetTeamUnits(teamID)
-		for i=1,#units do
-			local unitID = units[i]
-			if Spring.GetUnitSelfDTime(unitID) > 0 then
-				Spring.GiveOrderToUnit(unitID, CMD.SELFD, {}, 0)
+		for _,unitID in pairs(units) do
+			if (Spring.GetUnitSelfDTime(unitID) > 0) then
+				Spring.GiveOrderToUnit(unitID, CMD.SELFD, {}, {})
 			end
 		end
 	end
