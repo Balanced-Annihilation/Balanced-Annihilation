86a0a60d4777f36abe9c245ece94e62ae1343708
MasterBel2 <untakenprefix@gmail.com>
2020-11-16 10:00:04 +1000

    Replace “Shadow Quality Manager” with “Adaptive Graphics”
    Instead of changing shadow details, simply disable a number of different perf-hungry widgets.
    Will re-enable on shutdown.

---
+++ luaui/widgets/gfx_adaptive_graphics.lua
@@ -0,0 +1,66 @@
+
+function widget:GetInfo()
+return {
+    name      = "Adaptive graphics",
+    desc      = "adjust graphics for max fps based on fps",
+    author    = "",
+    date      = "0",
+    license   = "GNU GPL, v2 or later",
+    layer     = -10000,
+    enabled = true,
+	handler = true, 
+}
+end
+
+
+local turnedShadowsOff = false
+local spGetFPS				= Spring.GetFPS
+
+
+function widget:Shutdown()
+
+	local advgraphics = tonumber(Spring.GetConfigInt("advgraphics",1) or 1)
+	if(advgraphics == 1) then
+							Spring.SendCommands({"shadows 1 "..6144})
+
+				widgetHandler:EnableWidget("Projectile lights")
+				widgetHandler:EnableWidget("Deferred rendering")
+				widgetHandler:EnableWidget("Light Effects")
+				widgetHandler:EnableWidget("Contrast Adaptive Sharpen")
+				widgetHandler:EnableWidget("LupsManager")
+							Spring.SendCommands("AdvMapShading "..1)
+
+			Spring.SetConfigString("AdvMapShading", "1")
+			Spring.SendCommands("AdvModelShading "..1)
+			Spring.SetConfigString("AdvModelShading", "1")
+	end
+	
+	
+end
+
+local maxfpsmode = false
+
+function widget:GameFrame(gameFrame)
+		if not maxfpsmode then
+		if gameFrame > 1000 and gameFrame%500==0 then 
+			--local modelCount = #spGetVisibleUnits(-1,nil,false) + #spGetVisibleFeatures(-1,nil,false,false) -- expensive
+
+			
+			
+			if spGetFPS() <= 15 and (not maxfpsmode)  then
+						maxfpsmode = true
+						Spring.SendCommands("AdvMapShading "..0)
+						Spring.SendCommands("AdvModelShading "..0)
+														Spring.SendCommands({"shadows 0"})
+
+						widgetHandler:DisableWidget("Projectile lights")
+						widgetHandler:DisableWidget("Deferred rendering")
+						widgetHandler:DisableWidget("Light Effects")
+						widgetHandler:DisableWidget("Contrast Adaptive Sharpen")
+						widgetHandler:DisableWidget("LupsManager")
+			end
+		end
+		end
+end
+	
+
