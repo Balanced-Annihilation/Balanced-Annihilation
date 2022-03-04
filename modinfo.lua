0ea26ebd3af8a508440397fed53dfa21f57bc73a
Ares <noreply@noreply.com>
2022-03-04 22:35:21 +0000

    VERSION{v14.6.6}

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Balanced Annihilation',
   description='Balanced Annihilation',
   shortname='BA',
-  version='V15.0.7',
+  version='V14.6.6',
   mutator='Official',
   game='Balanced Annihilation',
   shortGame='BA',
