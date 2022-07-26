7144b6ef9ad6987ab7f7ba4e1f576ec010896711
CommanderSpice <commanderspice@braincalibration.de>
2022-07-27 00:59:25 +0200

    VERSION{v15.2.7}

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Balanced Annihilation',
   description='Balanced Annihilation',
   shortname='BA',
-  version='V15.2.6',
+  version='V15.2.7',
   mutator='Official',
   game='Balanced Annihilation',
   shortGame='BA',
