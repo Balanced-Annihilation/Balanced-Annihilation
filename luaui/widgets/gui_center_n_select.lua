caa3a1c8fdd045d5545b18ff3161d440a70d38fc
CommanderSpice <commanderspice@braincalibration.de>
2022-12-23 15:15:32 +0100

    Cleanup 'Select n Center!'

--- luaui/widgets/gui_center_n_select.lua
+++ luaui/widgets/gui_center_n_select.lua
@@ -1,8 +1,3 @@
--- $Id$
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-
 function widget:GetInfo()
   return {
     name      = "Select n Center!",
@@ -11,54 +6,16 @@ function widget:GetInfo()
     date      = "19 April 2008",
     license   = "GNU GPL, v2 or later",
     layer     = 5,
-    enabled   = true,  --  loaded by default?
-	handler = true
+    enabled   = true,
   }
 end
 
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-
-local go = true
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-
-function widget:Update()
-  local t = Spring.GetGameSeconds()
-  if (select(3,Spring.GetPlayerInfo(Spring.GetMyPlayerID(),false)) or t > 10) then
-    widgetHandler:RemoveWidget(self)
-    return
-  end
-  if (t > 0) then
+function widget:GameStart()
     local unitArray = Spring.GetTeamUnits(Spring.GetMyTeamID())
-    if (go and unitArray[1]) then
-      local x, y, z = Spring.GetUnitPosition(unitArray[1])
-	  
-	  value = tonumber(Spring.GetConfigInt("smoothcam", 1))
-		if value == 1 then
-					widgetHandler:DisableWidget("SmoothCam")
-			Spring.SetCameraTarget(x, y, z)
-			widgetHandler:EnableWidget("SmoothCam")
-		else
-			Spring.SetCameraTarget(x, y, z)
-		end
-     
-	  Spring.SetCameraTarget(x, y, z)
-
-	  
-      Spring.SelectUnitArray{unitArray[1]}
-      go = false
+    if unitArray[1] then
+        Spring.SelectUnitArray({unitArray[1]})
+        local x, y, z = Spring.GetUnitPosition(unitArray[1])
+        Spring.SetCameraTarget(x, y, z)
     end
-    if (not go) then
-      widgetHandler:RemoveWidget(self)
-    end
-  end
+    widgetHandler:RemoveWidget(self)
 end
-
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
